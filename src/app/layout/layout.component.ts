c09a1c07d78fe85abeb2a874bc9baa03de2aad57
Pankaj Meshram <pankaj_meshram@persistent.co.in>
2018-01-11 16:21:18 +0530

    Added the inactivity timeout functionality to layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+    t: any;
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.idleTimer();
+    }
+
+     idleTimer() {
+        // window.onload = resetTimer;
+        window.onmousemove = resetTimer; // catches mouse movements
+        window.onmousedown = resetTimer; // catches mouse movements
+        window.onclick = resetTimer;     // catches mouse clicks
+        window.onscroll = resetTimer;    // catches scrolling
+        window.onkeypress = resetTimer;
+
+        function logout() {
+            localStorage.setItem('isLoggedIn', 'false');
+            window.location.href = '/login';
+        }
+
+       function reload() {
+              window.location.href = self.location.href;
+       }
+
+       function resetTimer() {
+            clearTimeout(this.t);
+            this.t = setTimeout(logout, 3000);  // time is in milliseconds (1000 is 1 second)
+            this.t = setTimeout(reload, 5000);  // time is in milliseconds (1000 is 1 second)
+        }
+    }
+    
 }
